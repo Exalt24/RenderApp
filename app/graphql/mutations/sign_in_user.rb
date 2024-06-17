243778c7612acb19d270f22ef6858c2560362688
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-06-17 18:36:07 +0800

    Add existing project files to Git

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -10,20 +10,25 @@ module Mutations
     def resolve(credentials: nil)
       # basic validation
       return unless credentials
-
+    
       user = User.find_by email: credentials[:email]
-
+    
       # ensures we have the correct user
       return unless user
       return unless user.authenticate(credentials[:password])
-
+    
+      # Debug statement
+      secret_key_base = Rails.application.credentials.secret_key_base
+      raise "Secret key base is nil" if secret_key_base.nil?
+      puts "Secret key base: #{secret_key_base}"
+    
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
-      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-      token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+      crypt = ActiveSupport::MessageEncryptor.new(secret_key_base.byteslice(0..31))
+      token = crypt.encrypt_and_sign("user-id:#{user.id}")
       
       context[:session][:token] = token
-
+    
       { user: user, token: token }
-    end
+    end    
   end
 end
