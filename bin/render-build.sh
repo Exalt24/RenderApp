38246b7fafa8e8ecabf8f9cee2976b4f161e3282
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-06-17 22:07:41 +0800

    Add existing project files to Git

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# Set RAILS_ENV explicitly to avoid production mode issues
-export RAILS_ENV=production
-
 # Install all required gems specified in Gemfile.lock
 bundle install
 
