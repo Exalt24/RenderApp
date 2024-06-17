b93a7b52da2dd2b606656a05cea861f5ac5c6c93
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-06-17 21:08:06 +0800

    Add existing project files to Git

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+# Set RAILS_ENV explicitly to avoid production mode issues
+export RAILS_ENV=development
+
+# Install all required gems specified in Gemfile.lock
 bundle install
+
+# Precompile assets for production
 bundle exec rails assets:precompile
+
+# Clean up assets after precompilation (optional, but can be useful to reduce disk space usage)
 bundle exec rails assets:clean
