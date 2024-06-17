27da2ed081099481ade746bfbd9d722981805fbe
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-06-17 19:59:24 +0800

    Add existing project files to Git

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-RAILS_ENV=test bundle exec rails db:migrate  # Ensure test database schema is up to date
-RAILS_ENV=test bundle exec rails test  # Run all tests in the test environment
 
 echo "Build completed successfully."
